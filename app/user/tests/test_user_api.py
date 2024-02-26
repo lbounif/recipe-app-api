3a5c1622c426bed2fec0ed653952b238e13afe49
Lynda Bounif <l_bounif@esi.dz>
2024-02-26 11:39:02 -0500

    write tests for manage user API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -140,7 +140,7 @@ class PrivateUserApiTests(TestCase):
 
     def test_post_me_not_allowed(self):
         """Test POST is not allowed for the me endpoint."""
-        res = self.client.get(ME_URL)
+        res = self.client.post(ME_URL, {})
 
         self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
@@ -151,9 +151,9 @@ class PrivateUserApiTests(TestCase):
             'password': 'newpassword123',
         }
         
-        res = self.client.path(ME_URL, payload)
+        res = self.client.patch(ME_URL, payload)
 
         self.user.refresh_from_db()
         self.assertEqual(self.user.name, payload['name'])
-        self.assertEqual(self.user.password, payload['password'])
+        self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
